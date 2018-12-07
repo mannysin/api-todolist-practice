15833d277a8cf800e0528895a7dd4210d15aae1e
mannysin <sinclair2610@gmail.com>
2018-12-07 11:26:42 -0500

    building initial app

---
+++ routes/task-routes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router  = express.Router();
+const Task    = require('../models/Task')
+
+/* GET home page */
+router.get('/tasks', (req, res, next) => {
+  
+  Task.find()
+  .then((allTheTasks)=> {
+    res.json(allTheTasks)
+  })
+  .catch((err)=> {
+    res.json(err)
+  })
+});
+
+router.get('/tasks/details/:id', (req, res, next)=> {
+  Task.findById(req.params.id)
+  .then((theTask)=> {
+    res.json(theTask)
+  })
+  .catch((err)=> {
+    next(err)
+  })
+})
+
+router.post('/tasks/add-new', (req, res, next)=> {
+  Task.create({
+    title: req.body.theTitle,
+    description: req.body.theDescription
+  })
+  .then((theThingIAmGettingBack)=> {
+    res.json(theThingIAmGettingBack)
+  })
+  .catch((err)=> {
+    next(err)
+  })
+})
+
+router.post('/tasks/edit/:id', (req, res, next)=> {
+  Task.findByIdAndUpdate(req.params.id, {
+    title: req.body.theTitle,
+    description: req.body.theDescription
+  })
+  .then((theThingIAmGettingBack)=> {
+    res.json([{message: 'this task has been successfully updated'}], theThingIAmGettingBack)
+  })
+  .catch((err)=> {
+    next(err)
+  })
+})
+
+module.exports = router;
